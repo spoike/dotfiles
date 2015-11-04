9f766f915192e576b3818c0bdf7671d234f53c4e
Mikael Brassman <mishakun@gmail.com>
2015-11-04 08:03:58 +0100

    Install brew if missing

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -5,9 +5,16 @@ source ./scripts/funcs.sh
 
 msg "Setting up OSX"
 
+# XCode Command-line Tools
 l=$(xcode-select -p)
 if [ ! $? -eq 0 ]; then
   xcode-select --install
 else
-  ok "Command line tools"
+  ok "XCode Command-line Tools"
+fi
+
+if [ ! -x "$(command -v brew)" ]; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  ok "Homebrew (brew)"
 fi
