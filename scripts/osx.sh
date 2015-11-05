58ec3c04c85e01b0cb243f9e28fe03abd5893ac0
Mikael Brassman <mishakun@gmail.com>
2015-11-05 11:19:25 +0100

    Adding xcode-select warning for old OS Xs

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -6,28 +6,32 @@ source ./scripts/funcs.sh
 msg "Setting up OSX"
 
 # XCode Command-line Tools
-l=$(xcode-select -p)
-if [ ! $? -eq 0 ]; then
-  xcode-select --install
+if [ ! -x "$(command -v xcode-select)" ]; then
+  warn "Please install XCode Command-line Tools in order to install homebrew and its packages."
 else
-  ok "XCode Command-line Tools"
-fi
+  l=$(xcode-select -p)
+  if [ ! $? -eq 0 ]; then
+    xcode-select --install
+  else
+    ok "XCode Command-line Tools"
+  fi
 
-if [ ! -x "$(command -v brew)" ]; then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-  ok "Homebrew (brew)"
-fi
+  if [ ! -x "$(command -v brew)" ]; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  else
+    ok "Homebrew (brew)"
+  fi
 
-packages=()
-[ ! -d "/usr/local/opt/coreutils/libexec" ] && packages+=(coreutils)
-[ ! -x "$(command -v git)" ] && packages+=(git)
-[ ! -x "$(command -v tig)" ] && packages+=(tig)
-[ ! -x "$(command -v jq)" ] && packages+=(jq)
-[ ! -x "$(command -v wget)" ] && packages+=(wget)
-[ ! -x "$(command -v tmux)" ] && packages+=(tmux)
-if [ $packages ]; then
-  warn "Missing packages: ${packages}. Attempting to install with brew."
-  brew update
-  brew install $packages
+  packages=()
+  [ ! -d "/usr/local/opt/coreutils/libexec" ] && packages+=(coreutils)
+  [ ! -x "$(command -v git)" ] && packages+=(git)
+  [ ! -x "$(command -v tig)" ] && packages+=(tig)
+  [ ! -x "$(command -v jq)" ] && packages+=(jq)
+  [ ! -x "$(command -v wget)" ] && packages+=(wget)
+  [ ! -x "$(command -v tmux)" ] && packages+=(tmux)
+  if [ $packages ]; then
+    warn "Missing packages: ${packages}. Attempting to install with brew."
+    brew update
+    brew install $packages
+  fi
 fi
