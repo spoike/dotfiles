1651016f71c82dc90f4265894480ee817d740260
Mikael Brassman <mikael.brassman@tretton37.com>
2015-11-09 16:25:43 +0100

    Added atom installer script

---
+++ atom/atom.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+
+source $HOME/dotfiles/scripts/funcs.sh
+
+installed=`apm ls -ib`
+packages=()
+list=(`cat atom.packages`)
+for package in $list; do
+  if [[ ! -n "${installed[(r)${package}]}" ]]; then
+    warn "$package"
+    packages+="$package"
+  else
+    ok "$package"
+  fi
+done
+
+if [[ ! -z $packages ]]; then
+  msg "Will install missing Atom packages..."
+  for package in $packages; do
+    apm install ${package}
+  done
+else
+  msg "All atom packages are installed"
+fi
