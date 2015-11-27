96acaabca8cdea029dfbf65d45e1f9332d19d1c7
Mikael Brassman <mikael.brassman@tretton37.com>
2015-11-27 13:42:42 +0100

    Added useful tools to linux

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -13,6 +13,8 @@ if [[ -f /etc/debian_version ]]; then
   [ ! -x "$(command -v wget)" ] && packages+=(wget) || ok wget
   [ ! -x "$(command -v tmux)" ] && packages+=(tmux) || ok tmux
   [ ! -x "$(command -v xsel)" ] && packages+=(xsel) || ok xsel
+  [ ! -x "$(command -v iotop)" ] && packages+=(iotop) || ok iotop
+  [ ! -x "$(command -v htop)" ] && packages+=(htop) || ok htop
   if [ ! -z "$packages" ]; then
     warn "Missing packages: ${packages}. Will now attempt to install with apt-get."
     sudo apt-get install $packages
