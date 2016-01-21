901812017f431fc25353f4d151304f1fa9576ee8
Mikael Brassman <mikael.brassman@tretton37.com>
2016-01-21 15:57:56 +0100

    Updated whats my ip script

--- castle/bin/what-is-my-ip.sh
+++ castle/bin/what-is-my-ip.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
 # Pull out ip addy from ifconfig
 
-ifconfig | awk '/eth0/,/Scope:Link/' | awk 'match($0, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/) { print substr($0, RSTART, RLENGTH) }'
+IP=$(ifconfig | awk '/eth0/,/Scope:Link/' | awk 'match($0, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/) { print substr($0, RSTART, RLENGTH) }' | tr -d '\n')
+printf %s "$IP"
