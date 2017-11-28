0f603552b6c7172d7262c7aaa5d66ecdecbdceec
Mikael Brassman <mishakun@gmail.com>
2017-11-28 17:03:46 +0100

    add tldr command

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -9,6 +9,7 @@ packages=()
 [ ! -x "$(command -v git-fire)" ] && packages+=(git-fire) || ok git-fire
 [ ! -x "$(command -v flow)" ] && packages+=(flow-bin) || ok flow
 [ ! -x "$(command -v tmpin)" ] && packages+=(tmpin) || ok tmpin
+[ ! -x "$(command -v tldr)" ] && packages+=(tldr) || ok tldr
 if [ ! -z "$packages" ]; then
   warn "Missing packages: ${packages}. Will now attempt to install with npm."
   npm install -g $packages
